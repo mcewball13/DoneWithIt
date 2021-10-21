d65d59e1eedcae8d6583e78f792054cd3e9b1e49
mcewball13 <mcewen1984@hotmail.com>
2021-10-21 07:48:47 -0700

    added develop and base template

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    Button,
+    Platform,
+    StatusBar,
+    Dimensions
+} from "react-native";
+import {useDimensions, useDeviceOrientation} from "@react-native-community/hooks";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    
+
+    return (
+      <View style={{
+          backgroundColor: "#fff",
+          flex: 1,
+      }}>
+          <View style={{
+              
+          }}/>
+          
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    },
 });

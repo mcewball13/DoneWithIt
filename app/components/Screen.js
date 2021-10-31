a5c5a668cec26e7728c3e849872f0e48834a0ed5
mcewball13 <mcewen1984@hotmail.com>
2021-10-31 11:29:23 -0700

    reverted back to no padding

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -5,7 +5,7 @@ import { SafeAreaView, StyleSheet, View } from "react-native";
 function Screen({ children, style }) {
     return (
         <SafeAreaView style={[styles.screen, style]}>
-            <View style={[styles.screen, style]}>{children}</View>
+            {children}
         </SafeAreaView>
     );
 }
